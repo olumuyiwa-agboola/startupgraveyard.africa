3bac97d7f432ff378da7cf9c4eaf9089065a0b7d
olumuyiwa-agboola <agboolaod@gmail.com>
2024-01-13 21:24:49 +0100

    Setup project & implement startup, category and country models

---
+++ src/shared/models/country.ts
@@ -0,0 +1,7 @@
+export class Country {
+    constructor(public name : string,
+        public count : number = 0
+        ) {
+
+    }
+}
---
+++ src/shared/models/startup.ts
@@ -0,0 +1,12 @@
+export class Startup {
+    constructor(public name : string,
+        public countries : string[],
+        public funding : number,
+        public description : string,
+        public timeline : string,
+        public category : string,
+        public reference : string
+        ) {
+
+    }
+}
